9a47efea770d78c7212d0271c9e0c821fa04018a
Di3ri <drd338@nyu.edu>
2024-04-09 15:11:52 -0400

    Svg Push

--- ImageLoader.iso/Assets/GameManager.cs
+++ ImageLoader.iso/Assets/GameManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 using SimpleJSON;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using SVGAssets;
 
 
 public class GameManager : MonoBehaviour
@@ -16,6 +17,8 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     Image image;
+
+    int x;
 
     private void Awake()
     {
@@ -30,8 +33,10 @@ public class GameManager : MonoBehaviour
 
     void JSONReceiver(JSONNode json)
     {
-        tx_Name.text = json[0]["name"];
-        string imageURL = json[0]["image"];
+        x = Random.RandomRange(0, json.Count);
+        tx_Name.text = json[x]["name"];
+        string imageURL = json[x]["image"];
+        Debug.Log(imageURL);
         StartCoroutine(DownloadImage(imageURL));
     }
 
@@ -42,13 +47,19 @@ public class GameManager : MonoBehaviour
 
         if(request.result != UnityWebRequest.Result.ConnectionError)
         {
-            Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+           Texture2D texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
             print(texture);
             print(image);
             image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0, 0));
         }
 
 
+    }
+
+    public void RandomCountry()
+    {
+        
+        //JSONLoader.jsonRefreshed += JSONReceiver;
     }
 
 }
